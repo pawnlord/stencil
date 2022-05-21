690fa33b0b7ea9939f700a64761c6c6f91feabcf
pawnlord <scruffduff321@gmail.com>
2022-05-20 21:56:36 -0400

    Add decryption, fix encryption algorithm

--- stenography.py
+++ stenography.py
@@ -23,22 +23,42 @@ if options["encrypt"]:
         original = out.read()
     inp = b""
     if options["input"] == "":
-        inp = input().encode('utf-8')
+        inp = input("message? ").encode('utf-8')
     else:
         with open(options["input"], "rb") as input_file:
             inp = input_file.read()
     start = -1
     for i in range(len(original)-2):
-        if original[i+1] == 0xFF and original[i] == 0xDA:
+        if original[i+1] == 0xDA and original[i] == 0xFF:
             start = i+2
+            break
     if start == -1:
         print("[stan] Does not seem to be a JPG or PNG file, moving on...")
     
     new = bytearray(original)
     for i in range(start, start+(len(inp)*8)):
-        new[i] = (original[i] | (inp[(i-start)//8] & (2**((i-start)%8)))) & ((inp[(i-start)//8] & (2**((i-start)%8)))  )
+        inpn = (i-start)//8
+        bitn = (i-start)%8
+        bit = (inp[inpn] >> bitn) & 1
+        new[i] = (original[i] | bit) & bit
     with open("output.jpg", "wb") as out:
         out.write(bytes(new))
 
 else:
-    
+    length = int(input("length? "))
+    original = b""
+    with open(options["file"], "rb") as out:
+        original = out.read()
+    start = -1
+    for i in range(len(original)-2):
+        if original[i+1] == 0xDA and original[i] == 0xFF:
+            start = i+2
+
+    message = bytearray()
+
+    for i in range(start, start+length*8):
+        if (i-start)%8 == 0:
+            message.append(0)
+        bit = original[i] & 1
+        message[-1] = (message[-1] | (bit<<((i-start)%8)) )
+    print(bytes(message))
